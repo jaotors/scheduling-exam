0f3e209f6d693f5d245e2064f1231924d2352a39
Joshua B. Turingan <jotu@splitmedialabs.com>
2020-08-02 02:14:10 +0800

    update initial commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './app';
 import * as serviceWorker from './serviceWorker';
-import './tailwind.output.css';
 
 ReactDOM.render(
   <React.StrictMode>
